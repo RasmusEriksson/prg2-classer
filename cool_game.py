1f6821484a8a0c5643e6532c4b1605d20485dfbc
RasmusEriksson <rasmus.eriksson@elev.ga.ntig.se>
2025-09-26 11:02:08 +0200

    started something

---
+++ cool_game.py
@@ -0,0 +1,53 @@
+
+#decides board size and creates the board
+game_dimensions = {"x" : 10,"y" : 10}
+
+board = []
+
+#makes the board as a matrix by looping through the x and y values of the game_dimensions
+for y in range(0,game_dimensions["y"]):
+    new_row = []
+    for x in range(0,game_dimensions["x"]):
+        new_row.append(".")
+    
+    board.append(new_row)
+
+
+#all of this is enemy nonsense
+# basic reusable class for enemies
+class enemy:
+    def __init__(self, name: str, visual: str, hp: int, atk: int, spd: int, defense: int, x:int,y:int):
+
+        #data that is consistent for all enemies
+        self.visual = visual
+        self.name = name
+
+        self.hp = hp
+        self.atk = atk
+        self.spd = spd
+        self.defense = defense
+
+        #position values
+        self.x = x
+        self.y = y
+        
+def spawn(instance: any, x, y):
+    if type(instance) is enemy:
+        instance.x = x
+        instance.y = y
+
+        board[y][x] = instance.visual
+
+
+#Game nonsense
+#Renders the full game board, adding spacing inbetween the objects to spread out the positions evenly in a visual grid
+def render_board():
+    for row in board:
+        for space in row:
+            space_filling = "   " + space + "   "
+            print(space_filling,end="")
+        print("\n")
+
+new_enemy = enemy("gogad","🥸",10,5,1,0,0,0)
+spawn(new_enemy,5,5)
+render_board()
