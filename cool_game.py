65f44d0ccfe079abcaab5e1fb3bd1fad99ec4d62
RasmusEriksson <rasmus.eriksson@elev.ga.ntig.se>
2025-10-03 11:17:46 +0200

    made enemies be able to move

--- cool_game.py
+++ cool_game.py
@@ -1,14 +1,22 @@
+from time import sleep
+
+
 
 #decides board size and creates the board
 game_dimensions = {"x" : 10,"y" : 10}
 
+EMPTY = "."
 board = []
+
+#clamp function, returns an int within provided range
+def clamp(value,min_val,max_val):
+    return max(min_val,min(value,max_val))
 
 #makes the board as a matrix by looping through the x and y values of the game_dimensions
 for y in range(0,game_dimensions["y"]):
     new_row = []
     for x in range(0,game_dimensions["x"]):
-        new_row.append(".")
+        new_row.append(EMPTY)
     
     board.append(new_row)
 
@@ -30,6 +38,13 @@ class enemy:
         #position values
         self.x = x
         self.y = y
+    def move(self,x_move,y_move):
+        #current_space
+        board[self.y][self.x] = EMPTY
+        #target_space
+        self.y = clamp(self.y + y_move, 0, game_dimensions["y"]-1)
+        self.x = clamp(self.x + x_move, 0, game_dimensions["x"]-1)
+        board[self.y][self.x] = self.visual
         
 def spawn(instance: any, x, y):
     if type(instance) is enemy:
@@ -48,6 +63,13 @@ def render_board():
             print(space_filling,end="")
         print("\n")
 
+
+
 new_enemy = enemy("gogad","🥸",10,5,1,0,0,0)
 spawn(new_enemy,5,5)
-render_board()
+
+
+while True:
+    render_board()
+    sleep(1)
+    new_enemy.move(1,0)
